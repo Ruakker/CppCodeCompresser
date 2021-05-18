50644e68988dd2219e8b38b7c81f5448acfc0205
Chocola4ever <625850057@qq.com>
2021-05-18 19:03:34 +0800

    add input

--- js/compresswithformat.js
+++ js/compresswithformat.js
@@ -1,20 +1,20 @@
-
-function compresswithformat(str){
-	return $compresswithformat($compress($compress(str)));
+function compresswithformat(str) {
+  return $compresswithformat($compress($compress(str)));
 }
 
 function $compresswithformat(str) {
-	var ret = '';
-	var cnt=0;
-	var max_len = 80;
-	for (var i=0; i<str.length; ++i,++cnt) {
-		if (str[i]=='\n') cnt=0;
-		ret += str[i];
-		if (cnt == max_len - 1) {
-			ret += '\\';
-			ret += '\n';
-			cnt = 0;
-		}
-	}
-	return ret;
+  var ret = "";
+  var maxlen=width.maxlen.value;
+  if (maxlen<2) maxlen=2;
+  var cnt = 0;
+  for (var i = 0; i < str.length; ++i, ++cnt) {
+    if (str[i] == "\n") cnt = 0;
+    ret += str[i];
+    if (cnt == maxlen - 1) {
+      ret += "\\";
+      ret += "\n";
+      cnt = 0;
+    }
+  }
+  return ret;
 }
