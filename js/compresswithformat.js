598174f8cd68246c14057e816c6172da7b4d8212
Chocola4ever <625850057@qq.com>
2021-05-18 19:42:35 +0800

    format

--- js/compresswithformat.js
+++ js/compresswithformat.js
@@ -4,8 +4,8 @@ function compresswithformat(str) {
 
 function $compresswithformat(str) {
   var ret = "";
-  var maxlen=width.maxlen.value;
-  if (maxlen<2) maxlen=2;
+  var maxlen = width.maxlen.value;
+  if (maxlen < 2) maxlen = 2;
   var cnt = 0;
   for (var i = 0; i < str.length; ++i, ++cnt) {
     if (str[i] == "\n") cnt = 0;
