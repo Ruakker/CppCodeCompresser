ead305c9d9ef8d2835a4bba64ae36adb3e7b30ba
Chocola4ever <625850057@qq.com>
2021-05-18 21:31:32 +0800

    formated

--- js/compress.js
+++ js/compress.js
@@ -1,50 +1,66 @@
-
 // Mivik 2020.5.15
 
 // Inspired by wxh's code from (https://floj.tech/submission/24731)
 
-var endl = '\n';
+// Enhanced by Ruakker on 2021.5.18
 
-String.prototype.isSpace = function() {
-	return this == ' ' || this == '\t' || this == '\n' || this == '\r' || this == '\v' || this == '\f';
-}
+var endl = "\n";
 
-String.prototype.isIdentifierPart = function() {
-	return ('a' <= this && this <= 'z') || ('A' <= this && this <= 'Z') || ('0' <= this && this <= '9') || this == '$' || this == '_';
-}
+String.prototype.isSpace = function () {
+	return (
+		this == " " ||
+		this == "\t" ||
+		this == "\n" ||
+		this == "\r" ||
+		this == "\v" ||
+		this == "\f"
+	);
+};
 
-String.prototype.cut = function(lef, rig, ind) {
+String.prototype.isIdentifierPart = function () {
+	return (
+		("a" <= this && this <= "z") ||
+		("A" <= this && this <= "Z") ||
+		("0" <= this && this <= "9") ||
+		this == "$" ||
+		this == "_"
+	);
+};
+
+String.prototype.cut = function (lef, rig, ind) {
 	return this.substring(lef[ind], rig[ind] + 1);
-}
+};
 
-String.prototype.safeIndexOf = function(ch, ind) {
-	while (ind != this.length && !(this[ind].isSpace())) {
+String.prototype.safeIndexOf = function (ch, ind) {
+	while (ind != this.length && !this[ind].isSpace()) {
 		if (this[ind] == ch) return ind;
 		++ind;
 	}
 	return -1;
-}
+};
 
 function check(a, b) {
-	return (a.isIdentifierPart() && b.isIdentifierPart()) ||
-		   ((a == ' + ' || a == '-') && a == b) ||
-		   (a == '/' && b == '*');
+	return (
+		(a.isIdentifierPart() && b.isIdentifierPart()) ||
+		((a == " + " || a == "-") && a == b) ||
+		(a == "/" && b == "*")
+	);
 }
 
 function processTrigraph(str) {
 	var trigraphs = {
-		'=': '#',
-		'/': '\\',
-		'\'': '^',
-		'(': '[',
-		')': ']',
-		'<': '{',
-		'>': '}',
-		'!': '|',
-		'-': '~'
+		"=": "#",
+		"/": "\\",
+		"'": "^",
+		"(": "[",
+		")": "]",
+		"<": "{",
+		">": "}",
+		"!": "|",
+		"-": "~",
 	};
 	for (ch in trigraphs) {
-		var ret = '';
+		var ret = "";
 		var lst = 0;
 		var pattern = "??" + ch;
 		while (lst !== str.length) {
@@ -61,54 +77,57 @@ function processTrigraph(str) {
 
 function processLineBreak(str) {
 	var lines = str.split(endl);
-	var ret = '';
+	var ret = "";
 	for (var i = 0; i < lines.length; ++i) {
 		var line = lines[i];
 		var j = line.length;
-		while ((--j) >= 0 && line[j].isSpace());
-		if (line[j] == '\\') ret += line.substring(0, j);
+		while (--j >= 0 && line[j].isSpace());
+		if (line[j] == "\\") ret += line.substring(0, j);
 		else ret += line + endl;
 	}
 	if (ret.length !== 0 && ret[ret.length - 1] == endl)
-		ret = ret.substring(0, ret.length - 1);
+	ret = ret.substring(0, ret.length - 1);
 	return ret;
 }
 
 function processMultilineCommentAndReplaceMark(str) {
-	var ret = '';
-	var stringBegin, esc = false;
+	var ret = "";
+	var stringBegin,
+	esc = false;
 	var inString = false;
 	var lst = 0;
 	for (var i = 0; i < str.length; ++i) {
 		if (inString) {
-			if ((!esc) && str[i] == stringBegin) inString = false;
-		} else if (str[i] == '\'' || str[i] == '"') {
+			if (!esc && str[i] == stringBegin) inString = false;
+		} else if (str[i] == "'" || str[i] == '"') {
 			inString = true;
 			stringBegin = str[i];
 		} else if (i + 1 < str.length) {
-			if (str[i] == '/' && str[i + 1] == '*') {
-				ret += str.substring(lst, i) + ' ';
-				i = str.indexOf('*/', i + 2) + 2;
-				lst = i;
-				if (i == -1) break;
-			} else if (str[i] == '%' && str[i + 1] == ':') {
-				ret += str.substring(lst, i) + "#";
-				lst = i += 2;
+			if (str[i] == "/" && str[i + 1] == "*") {
+			ret += str.substring(lst, i) + " ";
+			i = str.indexOf("*/", i + 2) + 2;
+			lst = i;
+			if (i == -1) break;
+			} else if (str[i] == "%" && str[i + 1] == ":") {
+			ret += str.substring(lst, i) + "#";
+			lst = i += 2;
 			}
 		}
-		esc = str[i] == '\\';
+		esc = str[i] == "\\";
 	}
 	ret += str.substring(lst);
 	return ret;
 }
 
-function compress(str) { return $compress($compress(str)); }
+function compress(str) {
+	return $compress($compress(str));
+}
 
 function $compress(str) {
-	var ret = '';
+	var ret = "";
 	var lst = 0;
-	var patternS = "R\"(";
-	var patternT = ")\"";
+	var patternS = 'R"(';
+	var patternT = ')"';
 	while (lst !== str.length) {
 		var ind = str.indexOf(patternS, lst);
 		if (ind === -1) break;
@@ -134,7 +153,7 @@ function compressSingle(str) {
 	var arr = str.split(endl);
 	var ret = "";
 	var forceNewline = true;
-	var last = '\0';
+	var last = "\0";
 	forLine: for (var w = 0; w < arr.length; ++w) {
 		var line = arr[w];
 		if (line.length === 0) continue;
@@ -145,16 +164,17 @@ function compressSingle(str) {
 			while (i != line.length && line[i].isSpace()) ++i;
 			if (i === line.length) break;
 			lef.push(i);
-			var stringBegin, esc = false;
+			var stringBegin,
+			esc = false;
 			var inString = false;
-			while (i != line.length && (inString || (!(line[i].isSpace())))) {
+			while (i != line.length && (inString || !line[i].isSpace())) {
 				if (inString) {
-					if ((!esc) && line[i] == stringBegin) inString = false;
-				} else if (line[i] == '"' || line[i] == '\'') {
+					if (!esc && line[i] == stringBegin) inString = false;
+				} else if (line[i] == '"' || line[i] == "'") {
 					inString = true;
 					stringBegin = line[i];
-				} else if (line[i] == '/' && i != line.length - 1) {
-					if (line[i + 1] == '/') {
+				} else if (line[i] == "/" && i != line.length - 1) {
+					if (line[i + 1] == "/") {
 						rig.push(i - 1);
 						lef.push(i);
 						rig.push(line.length - 1);
@@ -163,7 +183,7 @@ function compressSingle(str) {
 				}
 				++i;
 				if (i == line.length) break;
-				esc = line[i] == '\\';
+				esc = line[i] == "\\";
 			}
 			rig.push(i - 1);
 		}
@@ -171,33 +191,55 @@ function compressSingle(str) {
 		if (!n) continue;
 		var originForceNewline = forceNewline;
 		var originLast = last;
-		if (last != '\0') {
+		if (last != "\0") {
 			if (forceNewline) ret += endl;
-			else if (check(last, line[lef[0]])) ret += ' ';
+			else if (check(last, line[lef[0]])) ret += " ";
 		}
 		last = line[rig[n - 1]];
 		var j = 0;
-		if (line[lef[0]] == '#') {
+		if (line[lef[0]] == "#") {
 			if (!forceNewline) ret += endl;
 			forceNewline = true;
-			if (!line[lef[0] + 1]) line[--lef[j = 1]] = '#';
+			if (!line[lef[0] + 1]) line[--lef[(j = 1)]] = "#";
 			ret += line.cut(lef, rig, j);
-			if (line[lef[j] + 1] == 'd' && line.safeIndexOf('(', lef[j + 1]) === -1) {
-				ret += ' ' + line.cut(lef, rig, ++j);
-				if (j + 1 < n) ret += ' ' + line.cut(lef, rig, ++j);
+			if (line[lef[j] + 1] == "d" && line.safeIndexOf("(", lef[j + 1]) === -1) {
+				ret += " " + line.cut(lef, rig, ++j);
+				if (j + 1 < n) ret += " " + line.cut(lef, rig, ++j);
 			}
 			++j;
 		} else forceNewline = false;
 		for (; j < n; ret += line.cut(lef, rig, j++)) {
 			if (lef[j] + 2 <= line.length) {
-				if (line.substring(lef[j], lef[j] + 2) == '//') {
+				if (line.substring(lef[j], lef[j] + 2) == "//") {
 					if (j == 0) forceNewline = originForceNewline;
 					break;
 				}
 			}
-			if (j && check(line[rig[j - 1]], line[lef[j]])) ret += ' ';
+			if (j && check(line[rig[j - 1]], line[lef[j]])) ret += " ";
 		}
-		last = j? line[rig[j - 1]]: originLast;
+		last = j ? line[rig[j - 1]] : originLast;
+	}
+	return ret;
+}
+
+function compresswithformat(str) {
+	return $compresswithformat($compress($compress(str)));
+}
+
+function $compresswithformat(str) {
+	var ret = "";
+	var maxlen = width.maxlen.value;
+	if (maxlen == -1) return str;
+	if (maxlen < 2) maxlen = 2;
+	var cnt = 1;
+	for (var i = 0; i < str.length; ++i, ++cnt) {
+		if (str[i] == "\n") cnt = 0;
+		ret += str[i];
+		if (cnt == maxlen - 1) {
+			ret += "\\";
+			ret += "\n";
+			cnt = 0;
+		}
 	}
 	return ret;
 }
