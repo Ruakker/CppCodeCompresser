23edd1b7933894a22110f24841d44fa385f0f1c2
Chocola4ever <625850057@qq.com>
2021-05-19 08:33:23 +0800

    add copy

---
+++ js/copytoclipboard.js
@@ -0,0 +1,9 @@
+
+function copyToClip(content) {
+    var str = document.createElement('textarea');
+    str.value= content;
+    document.body.appendChild(str);
+    str.select();
+    document.execCommand("copy");
+    document.body.removeChild(str);
+}
